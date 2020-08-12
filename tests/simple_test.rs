80e6b4be05ca0b5bc5b7ef74477c11a4a5d90c89
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-08-12 06:19:16 +0200

    Improve testing

--- tests/simple_test.rs
+++ tests/simple_test.rs
@@ -1,5 +1,20 @@
+use std::convert::TryInto;
+
 #[test]
 fn it_works() {
-    assert_eq!(pure_rust_locales::fr_BE::LC_TIME::D_FMT, "%d/%m/%y");
-    assert_eq!(pure_rust_locales::fr_BE::LC_TIME::FIRST_WEEKDAY, 2_i64);
+    use pure_rust_locales::fr_BE;
+
+    assert_eq!(fr_BE::LC_TIME::D_FMT, "%d/%m/%y");
+    assert_eq!(fr_BE::LC_TIME::FIRST_WEEKDAY, 2_i64);
+}
+
+#[test]
+fn parsing_locales() {
+    use pure_rust_locales::Locale;
+
+    let locale: Locale = "fr_BE".try_into().unwrap();
+    assert_eq!(locale, Locale::fr_BE);
+    let locale_string = "fr_BE".to_string();
+    let locale: Locale = locale_string.as_str().try_into().unwrap();
+    assert_eq!(locale, Locale::fr_BE);
 }
