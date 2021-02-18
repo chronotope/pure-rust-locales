9477cddbd54bebc95a5502b6e22b6c0eaf15f746
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-02-18 18:55:28 +0100

    Generate API

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#![no_std]
-
-#[derive(Debug)]
-pub struct UnknownLocale;
-
-include!(concat!(env!("OUT_DIR"), "/locales.rs"));
+version https://git-lfs.github.com/spec/v1
+oid sha256:2937061c7a3c746d66840e3b7cbab438c7acef6951343c4810aabc1886d35719
+size 2150205
