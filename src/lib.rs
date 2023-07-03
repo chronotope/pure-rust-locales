483437a7e8bccf7795cb32844036a33b72e822a2
Paul Dicker <paul@pitdicker.nl>
2023-07-03 10:12:03 +0200

    Regenerate with Rust 1.70

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13e522bf7f4507f95b8d8f0033d701fc74d57d8062ac3470c4762cb3e8e0ecab
-size 1910588
+oid sha256:9ccbfe512bb22a5bae98a7f827d7fa27c3f1c5303f9ab830077c172649e523f6
+size 1910482
