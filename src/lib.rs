b3c331d6387e8951acaf4415b7bd2d5a9d3b0e43
Paul Dicker <paul@pitdicker.nl>
2023-09-23 21:11:56 +0200

    Derive `Eq` and `Hash` for `Locale`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c187a8d88ba7dce6ffc967f9a0aaf66a7ac24aaff98be85ba6f7888931c4e305
-size 2446616
+oid sha256:e06dc42faeb0377d6a129a7935399539154746fe9b24202a99fe7d828e96dbb5
+size 2473422
